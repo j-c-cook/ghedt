a43bacd8330d6dc30c74d9cea0b17789b9a0663f
j-c-cook <jack-c-cook@protonmail.com>
2022-10-15 13:34:13 -0500

    Relax hybrid min and max EFT result to 7 places

--- tests/test_ghe.py
+++ tests/test_ghe.py
@@ -189,8 +189,8 @@ class TestGHE(unittest.TestCase):
 
         max_HP_EFT, min_HP_EFT = ghe.simulate(method='hybrid')
 
-        self.assertAlmostEqual(37.97229212228275, max_HP_EFT)
-        self.assertAlmostEqual(16.989189768401793, min_HP_EFT)
+        self.assertAlmostEqual(37.97229212228275, max_HP_EFT, places=7)
+        self.assertAlmostEqual(16.989189768401793, min_HP_EFT, places=7)
 
         ghe.size(method='hybrid')
 
